08a6ab8b5f08b0b5907f9643dcc2bdd2c1cd1142
pavan <pavan.nikhilesh@ovaledge.com>
2020-10-26 23:06:39 +0530

    welcome to hello node

---
+++ hellonode/hello.js
@@ -0,0 +1,6 @@
+//node js is'nt a programming language everything we write in javascript code!! 
+console.log("Welcome to the class!!");
+/* console.log is not actually part of the javascript language it's always a runtime code 
+weather it comes to the chrome or nodejs*/ 
+
+//use node hello.js to run your single line script!!
